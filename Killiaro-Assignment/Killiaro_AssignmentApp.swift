937dfe3e567dc71c98ce639537c152b36bf4c4ff
Majid Khoshpour <khoshpour@gmail.com>
2022-01-29 10:51:26 +0330

    Initial Commit

---
+++ Killiaro-Assignment/Killiaro_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Killiaro_AssignmentApp.swift
+//  Killiaro-Assignment
+//
+//  Created by Majid Khoshpour on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Killiaro_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
